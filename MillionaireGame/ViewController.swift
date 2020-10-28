96d1a0c8f32c81d8328081e63b21223d36e01bbb
Fedor Filimonov <f.filimonov@gmail.com>
2020-10-28 18:20:44 +0300

    Initial commit

---
+++ MillionaireGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionaireGame
+//
+//  Created by Федор Филимонов on 28.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
